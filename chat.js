cfd113d6ab6077d541b44333fd79f3b0895cd759
Diego <streethead01@gmail.com>
2026-07-22 11:35:15 -0300

    Guardian Sync: 1 cambios [Antigravity / Portal-Cliente]

--- chat.js
+++ chat.js
@@ -668,7 +668,6 @@ function addQuickReplies(replies) {
             });
         } else {
             chip.addEventListener('click', () => {
-                quickRepliesHistory = [];
                 container.remove();
                 lastUserMessage = label;
                 lastUserIntent = detectUserIntent(label);
